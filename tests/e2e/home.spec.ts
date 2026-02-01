6a4ed927e727b53d99a830c8d4b9bfb19a4931d6
Grzegorz Łaganowski <g.laganowski94@gmail.com>
2026-02-01 13:17:44 +0100

    remove old e2e placeholder test

--- tests/e2e/home.spec.ts
+++
@@ -1,6 +0,0 @@
-import { expect, test } from "@playwright/test";
-
-test("home page loads", async ({ page }) => {
-  await page.goto("/");
-  await expect(page).toHaveTitle("Generator · Rotation On-Call Keeper");
-});
